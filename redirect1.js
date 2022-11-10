a644f6aee006b02dbbffd69d6fd023868e041360
harri665 <harrison.d.martin@gmail.com>
2022-11-10 15:12:00 -0700

    working

--- redirect1.js
+++ redirect1.js
@@ -1,9 +1,9 @@
-// const params = new Proxy(new URLSearchParams(window.location.search), {
-//     get: (searchParams, prop) => searchParams.get(prop),
-//   });
-//   // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-//   let value = params.index; // "some_value"
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  let value = params.index; // "some_value"
 
-// // setTimeout(() => {
-// //     // window.location.href = "https://gmoyer.github.io/csci1000-website/redirect/index.html?index=" + (parseInt(value)+1);
-// // }, 10);
+setTimeout(() => {
+    window.location.href = "https://gmoyer.github.io/csci1000-website/redirect/index.html?index=" + (parseInt(value)+1);
+}, 10);
