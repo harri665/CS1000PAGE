e415060de3336350b8c15159243e628473bd3fce
harri665 <harrison.d.martin@gmail.com>
2022-11-10 15:21:09 -0700

    more

--- redirect1.js
+++ redirect1.js
@@ -3,7 +3,11 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   });
   // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
   let value = params.index; // "some_value"
-
+  if(params.index == NaN) {
+    value = 1; 
+  }
+  document.getElementById("loading").innerHTML = "Loading: " + value; 
+  document.body.style.backgroundColor = Math.random(); 
 setTimeout(() => {
-    window.location.href = "https://gmoyer.github.io/csci1000-website/redirect/index.html?index=" + (parseInt(value)+1);
+    // window.location.href = "https://gmoyer.github.io/csci1000-website/redirect/index.html?index=" + (parseInt(value)+1);
 }, 10);
